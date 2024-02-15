4f40f8eeae153d8e9c3b6f340afc6fd0192cd73c
abhishek22331 <abhishek.immanent@gmail.com>
2024-02-15 18:03:16 +0530

    learn expect

--- src/test/error.rs
+++ src/test/error.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 pub fn all_error(){
-    let greeting_file_result = File::open("hello.txt");
+   //  let greeting_file_result = File::open("hello.txt").expect("hello.txt should be included in this project"); //insted of using panic we can use expect which will give error message "hello.txt should be included in this project" when hello.txt not present
+    let greeting_file_result: Result<File, std::io::Error> = File::open("hello.txt");
 
     let _greeting_file = match greeting_file_result {
         Ok(file) => file,
